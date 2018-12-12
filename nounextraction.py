36f7fb12bf6d4f2185b8665be8653efe6c48b1f1
j0hnsolo <45828104+j0hnsolo@users.noreply.github.com>
2018-12-12 12:15:25 -0500

    Add files via upload

---
+++ nounextraction.py
@@ -0,0 +1,18 @@
+import nltk
+
+File = open("cleanwikipedia.csv") 
+lines = File.read() 
+sentences = nltk.sent_tokenize(lines) 
+nouns = [] #empty to array to hold all nouns
+
+for sentence in sentences:
+     for word,pos in nltk.pos_tag(nltk.word_tokenize(str(sentence))):
+         if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
+             nouns.append(word)
+
+print(nouns)
+nouns1 = " ".join(nouns)
+
+f = open("john.txt","w")
+f.write(nouns1)
+f.close()
